85928a8feab9b7af8343d39e5c89aaa98100371f
haukatkinson <haukatkinson@aol.com>
2022-09-13 20:50:55 +0000

    landing page complete

---
+++ src/js/component/cards.js
@@ -0,0 +1,38 @@
+import React from "react";
+export const Cards = () => {
+
+return (<div class="card-group p-3">
+  <div class="card">
+    <img class="card-img-top" src="https://via.placeholder.com/150" alt="Card image cap"/>
+    <div class="card-body d-flex flex-column">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <button type="button" class="mt-auto btn btn-lg bg-primary text-light">Find Out More!</button>
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="https://via.placeholder.com/150" alt="Card image cap"/>
+    <div class="card-body d-flex flex-column">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      <button type="button" class="mt-auto btn btn-lg bg-primary text-light">Find Out More!</button>
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="https://via.placeholder.com/150" alt="Card image cap"/>
+    <div class="card-body d-flex flex-column">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      <button type="button" class="mt-auto btn btn-lg bg-primary text-light">Find Out More!</button>
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="https://via.placeholder.com/150" alt="Card image cap"/>
+    <div class="card-body d-flex flex-column">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text ">This card has supporting text below as a natural lead-in to additional content.</p>
+      <button type="button" class="mt-auto btn btn-lg bg-primary text-light">Find Out More!</button>
+    </div>
+  </div>
+</div>
+)}
---
+++ src/js/component/navbar.js
@@ -0,0 +1,21 @@
+import React from "react";
+export const Navbar = () => {
+
+return ( <nav class="navbar navbar-dark bg-dark">
+  <a class="navbar-brand" href="#"><span>Start BootStrap</span></a>
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+</nav>
+)}
